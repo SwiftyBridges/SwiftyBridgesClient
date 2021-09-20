ddf2d52232c07764a517fa949b17522d7dc0a8ee
Stephen Kockentiedt <stephen@kockentiedt.name>
2021-09-20 09:40:49 +0200

    Add errors property to API classes

--- Sources/SwiftyBridgesClient/API.swift
+++ Sources/SwiftyBridgesClient/API.swift
@@ -7,18 +7,75 @@
 
 import Foundation
 
-public protocol API {
-    init(baseRequest: URLRequest, client: BridgeClient)
+open class API {
+    private let baseRequest: URLRequest
+    private let client: BridgeClient
+    
+    private let errorContinuations = Continuations<Error>()
+    
+    public init(baseRequest: URLRequest, client: BridgeClient = .shared) {
+        self.baseRequest = baseRequest
+        self.client = client
+    }
+    
+    public func perform<Call: APIMethodCall>(_ call: Call) async throws -> Call.ReturnType {
+        do {
+            return try await client.perform(call, baseRequest: baseRequest)
+        } catch {
+            await errorContinuations.yield(error)
+            throw error
+        }
+    }
 }
 
 extension API {
-    public init(url: URL, client: BridgeClient = .shared) {
+    public convenience init(url: URL, client: BridgeClient = .shared) {
         self.init(baseRequest: URLRequest(url: url), client: client)
     }
     
-    public init(url: URL, bearerToken: String, client: BridgeClient = .shared) {
+    public convenience init(url: URL, bearerToken: String, client: BridgeClient = .shared) {
         var request = URLRequest(url: url)
         request.addValue("Bearer \(bearerToken)", forHTTPHeaderField: "Authorization")
         self.init(baseRequest: request, client: client)
+    }
+}
+
+extension API {
+    public var errors: AsyncStream<Error> {
+        AsyncStream { continuation in
+            Task {
+                await errorContinuations.add(continuation)
+            }
+        }
+    }
+}
+
+actor Continuations<Element> {
+    private var continuationByID: [UUID: AsyncStream<Element>.Continuation] = [:]
+    
+    deinit {
+        continuationByID.values.forEach { $0.finish() }
+    }
+    
+    func add(_ continuation: AsyncStream<Element>.Continuation) {
+        let id = UUID()
+        continuationByID[id] = continuation
+        continuation.onTermination = { @Sendable [weak self] _ in
+            guard let self = self else { return }
+            Task {
+                await self.remove(continuationWithID: id)
+            }
+        }
+    }
+    
+    func yield(_ value: Element) {
+        for continuation in continuationByID.values {
+            continuation.yield(value)
+        }
+    }
+    
+    private func remove(continuationWithID id: UUID) {
+        continuationByID[id]?.finish()
+        continuationByID[id] = nil
     }
 }
